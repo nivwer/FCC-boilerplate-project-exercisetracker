85f78dd763b8d45c07e3bdbef30c14235d74dc20
nivwer <nivwerr@gmail.com>
2023-07-16 11:35:21 -0300

    Endpoint: (/api/users), Add function createUser with post method.

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+require('./connection');
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const User = require("./models/Users");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.post("/api/users", (req, res) => {
+  const createUser = async (username) => {
+    const user = new User({
+      username: username,
+    });
+    await user.save();
+    return user;
+  };
 
+  createUser(req.body.username)
+    .then(user => res.json({
+        username: user.username,
+        _id: user._id,
+      }))
+    .catch(err => console.log(err))
+});
 
-
+/* app.get('/api/users', (req, res) => {
+  
+}) */
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
